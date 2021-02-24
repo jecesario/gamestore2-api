26791a18105595e792f842663cf3cfa88eb0751e
Jecesario <jp@soloid.com.br>
2021-02-24 11:00:18 -0300

    Adicionando funcionalidade para atualizar uma categoria

--- src/main/java/com/jecesario/gamestore/controllers/CategoriasController.java
+++ src/main/java/com/jecesario/gamestore/controllers/CategoriasController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,11 @@ public class CategoriasController {
 	@GetMapping("/buscarDescricao/{descricao}")
 	public ResponseEntity<List<Categorias>> getByDescricao(@PathVariable String descricao) {
 		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categorias> update(@RequestBody Categorias categoria) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
 	}
 
 }
