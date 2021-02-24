d51edd5541f8eb0d576c50cd59cfc87888b32178
Jecesario <jp@soloid.com.br>
2021-02-24 11:09:15 -0300

    Adicionando a funcionalidade de deletar categorias

--- src/main/java/com/jecesario/gamestore/controllers/CategoriasController.java
+++ src/main/java/com/jecesario/gamestore/controllers/CategoriasController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,7 +36,7 @@ public class CategoriasController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
 	}
 
-	@GetMapping("/buscarId/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Categorias> getById(@PathVariable long id) {
 		return repository.findById(id).map(obj -> ResponseEntity.ok(obj)).orElse(ResponseEntity.notFound().build());
 	}
@@ -48,6 +49,11 @@ public class CategoriasController {
 	@PutMapping
 	public ResponseEntity<Categorias> update(@RequestBody Categorias categoria) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 
 }
