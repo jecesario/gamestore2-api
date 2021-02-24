4b671024b935e18c745c342521ef2ff72c45e547
Jecesario <jp@soloid.com.br>
2021-02-24 12:54:37 -0300

    Adicionando funcionalidade de buscar todos os produtos

--- src/main/java/com/jecesario/gamestore/controllers/ProdutosController.java
+++ src/main/java/com/jecesario/gamestore/controllers/ProdutosController.java
@@ -1,12 +1,27 @@
 package com.jecesario.gamestore.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jecesario.gamestore.models.Produtos;
+import com.jecesario.gamestore.repositories.ProdutosRepository;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/produtos")
 public class ProdutosController {
-
+	
+	@Autowired
+	private ProdutosRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produtos>> list () {
+		return ResponseEntity.ok(repository.findAll());
+	}
 }
