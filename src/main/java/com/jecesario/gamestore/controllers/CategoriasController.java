54e686aa00cd5b143f9088b74ba385cd73b93163
Jecesario <jp@soloid.com.br>
2021-02-24 10:05:34 -0300

    Adicionando a funcionalidade de adicionar categorias

--- src/main/java/com/jecesario/gamestore/controllers/CategoriasController.java
+++ src/main/java/com/jecesario/gamestore/controllers/CategoriasController.java
@@ -3,9 +3,13 @@ package com.jecesario.gamestore.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +27,11 @@ public class CategoriasController {
 	@GetMapping
 	public ResponseEntity<List<Categorias>> list() {
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categorias> add (@RequestBody Categorias categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
 	}
 	
 }
