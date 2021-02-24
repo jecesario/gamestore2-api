e9a8fd46891660347e85d73266916c88c45f8ff9
Jecesario <jp@soloid.com.br>
2021-02-24 13:11:45 -0300

    Adicionando a funcionalidade de adicionar um produto

--- src/main/java/com/jecesario/gamestore/controllers/ProdutosController.java
+++ src/main/java/com/jecesario/gamestore/controllers/ProdutosController.java
@@ -3,9 +3,12 @@ package com.jecesario.gamestore.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +19,18 @@ import com.jecesario.gamestore.repositories.ProdutosRepository;
 @CrossOrigin("*")
 @RequestMapping("/produtos")
 public class ProdutosController {
-	
+
 	@Autowired
 	private ProdutosRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Produtos>> list () {
+	public ResponseEntity<List<Produtos>> list() {
 		return ResponseEntity.ok(repository.findAll());
 	}
+
+	@PostMapping
+	public ResponseEntity<Produtos> add(@RequestBody Produtos produto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
+
 }
