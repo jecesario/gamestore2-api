e496012296cf95e280948e66c13b12dcfd6089e1
Jecesario <jp@soloid.com.br>
2021-02-24 09:54:00 -0300

    Adicionada a funcionalidade de listar todas as categorias

--- src/main/java/com/jecesario/gamestore/controllers/CategoriasController.java
+++ src/main/java/com/jecesario/gamestore/controllers/CategoriasController.java
@@ -1,14 +1,28 @@
 package com.jecesario.gamestore.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jecesario.gamestore.models.Categorias;
+import com.jecesario.gamestore.repositories.CategoriasRepository;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping(value = "/categorias")
 public class CategoriasController {
 
+	@Autowired
+	private CategoriasRepository repository;
 	
+	@GetMapping
+	public ResponseEntity<List<Categorias>> list() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 	
 }
