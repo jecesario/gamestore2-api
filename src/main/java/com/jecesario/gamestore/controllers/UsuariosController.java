63cdaaa269a4c45bab64dd2601c32f9584d89ea3
Jecesario <jp@soloid.com.br>
2021-02-25 22:47:52 -0300

    Adicionando funcionalidade de atualizar usuario

--- src/main/java/com/jecesario/gamestore/controllers/UsuariosController.java
+++ src/main/java/com/jecesario/gamestore/controllers/UsuariosController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,11 @@ public class UsuariosController {
 	@GetMapping("/nome/{nome}")
 	public ResponseEntity<Usuarios> getByNome(@PathVariable String nome) {
 		return ResponseEntity.ok(repository.findByNomeContainingIgnoreCase(nome));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuarios> update (@RequestBody Usuarios usuario) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuario));
 	}
 
 }
